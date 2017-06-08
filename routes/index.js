21ec712e48f23db40325c44663f3f982f801133e
UF onBrand <onbrand@uberflip.com>
2017-06-07 21:54:58 -0400

    pre google

--- routes/index.js
+++ routes/index.js
@@ -18,26 +18,30 @@ const fs = require('fs');
 
 
 router.get('/', (req, res) => {
-  // res.render('layout');
-  console.log(req.body)
-  res.send('hello!')
+    // res.render('layout');
+    console.log(req.body)
+    res.send('hello!')
 });
 
 router.post('/', upload.single('img'), (req, res) => {
-	
-  // res.render('layout');
-  console.log(req.body)
-  console.dir(req.file);
 
-  //gives you the proper extension (IE .jpg, .mp4, etc) based on mimetype
-  const fileExtension = mime.extension(req.file.mimetype);
-  
-  //rename file with appropriate extension (fs = filesystem)
-  fs.rename(`uploads/${req.file.filename}`, `uploads/${req.file.filename}.${fileExtension}`, function(err) {
-    if ( err ) console.log('ERROR: ' + err);
-});
+    // res.render('layout');
+    console.log(req.body)
+    console.dir(req.file);
 
-  res.send(`Thank you for uploading ${req.file.originalname}, it has been renamed to ${req.file.filename}.${fileExtension} in the uploads folder.`)
+    //gives you the proper extension (IE .jpg, .mp4, etc) for your file based on its mimetype
+    const fileExtension = mime.extension(req.file.mimetype);
+
+    //rename file with appropriate extension (fs = filesystem)
+    //(arg1 = current file path, arg2 = location of renamedfile/its new name)
+    fs.rename(`uploads/${req.file.filename}`, `uploads/${req.file.filename}.${fileExtension}`, function(err) {
+        if (err) {
+            console.log('ERROR: ' + err);
+        }
+    });
+
+    //give feedback to client
+    res.send(`Thank you for uploading ${req.file.originalname}, it has been renamed to ${req.file.filename}.${fileExtension} in the uploads folder.`)
 });
 
 module.exports = router;
