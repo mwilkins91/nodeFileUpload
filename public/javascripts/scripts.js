efdcaa4588c2ae498c3a33dc15e1d1469af8900b
UF onBrand <onbrand@uberflip.com>
2017-06-13 15:18:50 -0400

    tabs functionality test

--- public/javascripts/scripts.js
+++ public/javascripts/scripts.js
@@ -12,12 +12,22 @@ var continueButton = function() {
 	})
 }
 
-var nav
+function tabs() {
+	var $tabs = $('a[role="tab"]');
+	$tabs.on('click', function() {
+		var sectionId = $(this).attr('aria-controls');
+		$contentSection.attr('aria-hidden', 'true');
+		$tabs.attr('aria-selected', 'false');
+		$(this).attr('aria-selected', 'true');
+		$(`section#${sectionId}`).attr('aria-hidden', 'false');
+
+	})
+}
 
 
 // import { $, $$ } from './modules/bling';
 $(function() {
 	continueButton();
-
+	tabs();
 
 })
